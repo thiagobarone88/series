cca6548625d457a8a3db976e2a7272d7709d420b
thiagobarone88 <thiagobarone88@hotmail.com>
2021-02-17 22:39:56 -0300

    First commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace Series.Classes
+{
+    public enum Genero
+    {
+        
+        Acao = 1,
+
+        Terror = 2,
+
+        Drama = 3,
+
+        Desenho = 4,
+
+        Suspense = 5,
+
+        Comedia = 6
+
+    }
+}
